ec8915ba792635537d207e3c860e61d12c401ca8
Rail <rjzakirov+1@gmail.com>
2019-08-30 07:14:00 +0200

    Первый комит по курсу

---
+++ Python: основы и применение/1_Basic/1_2_1.py
@@ -0,0 +1,5 @@
+x = [1,2,3]
+for i in x:
+    print(id(i))
+print(id(x))
+print(id([1,2,3]))
---
+++ Python: основы и применение/1_Basic/1_2_2_print.py
@@ -0,0 +1,10 @@
+x = [1,2,3]
+y = x
+y.append(4)
+
+s = '123'
+t = s
+t = t + "4"
+
+print(str(x) + " " + s)
+
